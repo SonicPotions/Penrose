cdba0f9f09f3e8469decf7c693011a5bbe26d385
Julian Schmidt <julian@sonic-potions.com>
2015-01-01 22:35:03 +0100

    fixed fuses to use the external osc

--- Bootloader/AudioBoot_V2_0/Atmega_Source/AudioBoot/flashAvr.sh
+++ Bootloader/AudioBoot_V2_0/Atmega_Source/AudioBoot/flashAvr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # set fuses
-avrdude   -B100 -c usbasp -p atmega168 -U lfuse:w:0xe2:m -U hfuse:w:0xd4:m -U efuse:w:0x02:m
+avrdude   -B100 -c usbasp -p atmega168 -U lfuse:w:0xff:m -U hfuse:w:0xd4:m -U efuse:w:0x02:m
 # flash bootloader
-avrdude  -B1  -c usbasp -p atmega168 -U flash:w:audioBootloader.hex:i
+#avrdude  -B1  -c usbasp -p atmega168 -U flash:w:audioBootloader.hex:i
+avrdude  -B1  -c usbasp -p atmega168 -U flash:w:./combined/combinedQuantizer.hex:i
 # lock bootloader - BLB1 Mode 2 = LPM and SPM is not allowed to write to the Boot Loader Section
 avrdude  -B1  -c usbasp -p atmega168 -U lock:w:0xCF:m 
